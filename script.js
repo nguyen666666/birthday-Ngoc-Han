fe1aa2413f077d602547fa3ecda57d44876e9dcb
nguyen666666 <ncac0200@gmail.com>
2025-05-25 23:37:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const messages = {
   1: "Từ Anh: Anh luôn nhớ khoảnh khắc đầu tiên chúng ta nói chuyện – em còn chả thèm rep, và rep chậm điên. Nhưng không sao em vẫn là cô gái anh thật sự chú ý đến.",
   2: "Từ Anh: Có những ngày không lý do đặc biệt, nhưng chỉ cần nghĩ đến em, Anh đã thấy ấm áp lạ kỳ(câu này anh bịa copy trên mạng á).",
   3: "Từ Anh: Nếu có điều ước, Anh ước mình có thể bên em trong những lúc em mỉm cười, và cả khi em buồn, này real nha không xạo.",
-  4: "Từ Anh (Lá thư cuối): Em à, Anh biết điều này có thể đến bất ngờ, nhưng thật lòng, Anh thích em. Anh không biết tương lai ra sao, nhưng hôm nay – ngày đặc biệt của em – Anh muốn em biết: có một người luôn dõi theo em, âm thầm và chân thành. I Love You 3000 ❤️"
+  4: "Từ Anh (Lá thư cuối): Em à, anh nghĩ anh thật sự thích em lắm, tuy dạo gần đây ai cũng bận không thể dành nhiều thời gian cho nhau tuy vậy anh muốn em biết là em là duy nhất, số 1 trong mắt anh kkk và cuối cùng là anh chúc em sinh nhật vui vẻ, ngủ mơ được ăn 10 cái bánh kem hải sản super max, và mong em tuổi mới nếu thức khuya thì lùn đi 6cm, thế nhá baby"
 };
 
 function openLetter(id) {
