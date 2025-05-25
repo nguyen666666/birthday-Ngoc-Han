528314ceacb848e86c0fb3f60aa5abd41ee41ba7
nguyen666666 <ncac0200@gmail.com>
2025-05-25 23:25:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const messageBox = document.getElementById("messageBox");
 const messageText = document.getElementById("messageText");
 
 const messages = {
-  1: "Từ Nguyên: Tớ luôn nhớ khoảnh khắc đầu tiên mình nói chuyện – thật tự nhiên, thật dễ thương. Ngày đó, và cả bây giờ, cậu vẫn luôn dịu dàng như thế.",
-  2: "Từ Nguyên: Có những ngày chẳng có lý do gì đặc biệt, nhưng chỉ cần nghĩ đến tên cậu, tớ đã thấy ấm áp lạ kỳ.",
-  3: "Từ Nguyên: Nếu có điều ước, tớ ước mình có thể bên cậu trong những lúc cậu mỉm cười, và cả khi cậu buồn.",
-  4: "Từ Nguyên (Lá thư cuối): Hân à, tớ biết có thể điều này đến bất ngờ, nhưng thật lòng, tớ thích cậu. Tớ không biết tương lai sẽ ra sao, nhưng hôm nay – ngày đặc biệt của cậu – tớ muốn cậu biết: có một người luôn dõi theo cậu, âm thầm, và chân thành. I Love You 3000 ❤️"
+  1: "Từ Anh: Anh luôn nhớ khoảnh khắc đầu tiên chúng ta nói chuyện – em còn chả thèm rep, và rep chậm điên. Nhưng không sao em vẫn là cô gái anh thật sự chú ý đến.",
+  2: "Từ Anh: Có những ngày không lý do đặc biệt, nhưng chỉ cần nghĩ đến em, Anh đã thấy ấm áp lạ kỳ(câu này anh bịa copy trên mạng á).",
+  3: "Từ Anh: Nếu có điều ước, Anh ước mình có thể bên em trong những lúc em mỉm cười, và cả khi em buồn, này real nha không xạo.",
+  4: "Từ Anh (Lá thư cuối): Em à, Anh biết điều này có thể đến bất ngờ, nhưng thật lòng, Anh thích em. Anh không biết tương lai ra sao, nhưng hôm nay – ngày đặc biệt của em – Anh muốn em biết: có một người luôn dõi theo em, âm thầm và chân thành. I Love You 3000 ❤️"
 };
 
 function openLetter(id) {
